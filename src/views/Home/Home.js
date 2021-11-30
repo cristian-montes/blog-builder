0d34be8bc2f784fd13764470eb8724eb2543b25c
Cristian Montes <cmontesmujica@gmail.com>
2021-11-29 19:25:40 -0700

    font and aligment working

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -5,13 +5,30 @@ import './Home.css'
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
+
   const [title, setTitle] = useState('')
   const [subtitle, setSubtitle] = useState('')
+  const [text, setText] = useState('')
+  const [font, setFont] = useState('')
+  const [align, setAlign] = useState('')
+
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle} />
-      <Editor title={title} setTitle={setTitle} subtitle={subtitle} setSubtitle={setSubtitle} />
+
+      <Preview title={title} subtitle={subtitle} text={text} font={font} align={align} />
+      <Editor
+        title={title}
+        setTitle={setTitle}
+        subtitle={subtitle}
+        setSubtitle={setSubtitle}
+        text={text}
+        setText={setText}
+        font={font}
+        setFont={setFont}
+        align={align}
+        setAlign={setAlign}
+      />
     </main>
   )
 }
